7cca23abe591fcf439a6c71a982bfdc345a7f551
Gobinda Malakar <gobindamalakar68@gmail.com>
2023-05-03 23:56:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -17,7 +17,13 @@ const library = [
 ];
 
 const numberOfBooksRead = () => {
-  // write your code here
+  const c = 0;
+  for(let i=0;i<llibrary.length;i++){
+	  if(library[readingStatus] == true){
+		  c++;
+	  }
+  }
+	return c;
 };
 
 // Do not change the code below
